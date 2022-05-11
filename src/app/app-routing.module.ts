3ab0d2ff091aafdb510c783d57125ab1b39ca4f1
Saulid <lwinh80@gmail.com>
2022-05-11 16:36:46 +0630

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { UserDetailsComponent } from './user-details/user-details.component';
